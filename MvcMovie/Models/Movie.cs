c7609812c7db638c5a2edd75c13daece0d7adbdf
Junyang Zhang <zhangjunyang94@gmail.com>
2020-01-08 17:48:35 +0800

    1完成分类搜索 2添加新字段

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -14,9 +14,11 @@ namespace MvcMovie.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
     public class MovieDBContext : DbContext
     {
+       
         public DbSet<Movie> Movies { get; set; }
     }
 }
