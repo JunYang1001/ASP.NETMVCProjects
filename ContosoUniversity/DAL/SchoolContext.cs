60a90b759a0a6308966a154327da6bec5a5abeb6
Junyang Zhang <zhangjunyang94@gmail.com>
2020-01-15 11:50:15 +0800

    实现继承

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -14,6 +14,7 @@ namespace ContosoUniversity.DAL
         {
 
         }
+        public DbSet<Person> People { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
